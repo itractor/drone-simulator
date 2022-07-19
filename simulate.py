3c814f7d52d846e26986ec24f9f16eb36e99a304
oystein <oebars@gmail.com>
2022-07-19 15:42:48 +0200

    generate data and push to kafka

--- simulate.py
+++ simulate.py
@@ -2,46 +2,33 @@
 """
 Module Docstring
 """
-
-__author__ = "Øystein"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
+import io
 import argparse
+import avro.schema
+import random
+import time
+from kafka import KafkaProducer
+from avro.io import DatumWriter
 
+SCHEMA = avro.schema.parse(open("drone-data.avsc").read())
 
-def main(args):
-    """ Main entry point of the app """
-    print("hello world")
-    print(args)
+def main():
+    producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+    while True:
+        writer = DatumWriter(SCHEMA)
+        bytes_writer = io.BytesIO()
+        encoder = avro.io.BinaryEncoder(bytes_writer)
+        time.sleep(1)
+        writer.write({"x": random.uniform(0, 10), "y": random.uniform(0, 10), "z": random.uniform(0, 10), "droneId": random.randint(1, 100)}, encoder)
+        producer.send('workers', bytes_writer.getvalue())
 
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser() #TODO add some arguments later
 
-    # Required positional argument
-    parser.add_argument("arg", help="Required positional argument")
-
-    # Optional argument flag which defaults to False
-    parser.add_argument("-f", "--flag", action="store_true", default=False)
-
-    # Optional argument which requires a parameter (eg. -d test)
-    parser.add_argument("-n", "--name", action="store", dest="name")
-
-    # Optional verbosity counter (eg. -v, -vv, -vvv, etc.)
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        action="count",
-        default=0,
-        help="Verbosity (-v, -vv, etc)")
-
-    # Specify output of "--version"
-    parser.add_argument(
-        "--version",
-        action="version",
-        version="%(prog)s (version {version})".format(version=__version__))
-
-    args = parser.parse_args()
-    main(args)
+    main()
